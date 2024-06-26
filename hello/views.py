aea3ae1da59697c13546122177e2c9bb3694d907
l-guilherme <luguibar@gmail.com>
2024-06-26 02:24:10 +0000

    Finalização do trabalho cloud

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django! <br> por Luiz Guilherme Barcellos <br> em 25/06/2024")
+    return HttpResponse("Hello, Django!<br> Cloud Computing & Site Reliability Engineering <br> "+
+                        "por Luiz Guilherme Barcellos <br> em 25/06/2024")
